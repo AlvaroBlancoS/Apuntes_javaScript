fbd6eb48f19bd854643c32c9e4a021b425a0cc25
alvaroblancosangines <alvaroblancones@gmail.com>
2023-11-19 13:22:47 +0100

    Como funciona el control de estructura tanto la consola como las funciones y eventos

---
+++ ControlEstructura/consola/bucleFor.js
@@ -0,0 +1,9 @@
+let numero = parseInt(9);
+for (let i = 1; i <=10; i++) {
+    console.log(numero +" x "+i+"= "+(numero*i));
+}
+/*
+for(;;){
+    numero++;
+}
+*/
---
+++ ControlEstructura/consola/sentenciaIf.js
@@ -0,0 +1,6 @@
+let edad = -1;
+if (edad>=18) {
+    console.log("Eres mayor de edad, felicidades!!");
+}else{
+    console.log("Niño/a, vete con tus papás!!")
+}
